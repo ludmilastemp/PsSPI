efc9d51211b0d64a7cecafbab35e925fc59949ad
LudmilaStemp <StemporzheckayaLudmila@yandex.ru>
2024-11-29 17:42:32 +0300

    add tool action

--- PsSPI.hpp
+++ PsSPI.hpp
@@ -1,14 +1,26 @@
 #ifndef __PsSPI__
 #define __PsSPI__
 
+using tool_t = int;
+
+struct PsSPI_Tool
+{
+    tool_t id;
+    const char* png;
+    void (*activate) ();
+    void (*deActivate) ();
+    void (*apply) ();
+};
+
 class PsSPI
 {
 public:
 
-    void addTool (const char* png);
+    tool_t addTool (PsSPI_Tool tool);
 };
 
 PsSPI* getPsSPI ();
+
 void loadPlugin ();
 
 
--- simple.cpp
+++ simple.cpp
@@ -1,9 +1,40 @@
 #include "PsSPI.hpp"
 
+#include <iostream>
+
 PsSPI* psspi = nullptr;
+
+void activate   ();
+void deActivate ();
+void apply      ();
 
 void loadPlugin ()
 {
     psspi = getPsSPI ();
-    psspi->addTool ("img/brush.png");
+
+    PsSPI_Tool tool 
+    {
+        .id         = -1,
+        .png        = "img/brush.png",
+        .activate   = &activate,
+        .deActivate = &deActivate,
+        .apply      = &apply
+    };
+
+    tool.id = psspi->addTool (tool);
+}
+
+void activate ()
+{
+    std::cout << "meow" << std::endl;
+}
+
+void deActivate ()
+{
+    std::cout << "bye" << std::endl;
+}
+
+void apply ()
+{
+    std::cout << "la-la-la" << std::endl;
 }
