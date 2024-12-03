24326b8a8b0d9661e84b47dd9c31193364ab58af
LudmilaStemp <StemporzheckayaLudmila@yandex.ru>
2024-12-03 03:04:13 +0300

    add tool icon

--- PsSPI.hpp
+++ PsSPI.hpp
@@ -1,43 +1,18 @@
 #ifndef __PsSPI__
 #define __PsSPI__
 
-using tool_t = int;
-
-struct PsSPI_Tool
-{
-    tool_t id;
-    const char* png;
-    void (*activate) ();
-    void (*deActivate) ();
-    void (*apply) ();
-};
-
-struct PsSPI_Color
-{
-    unsigned char r, g, b, a;
-};
-
-struct PsSPI_Event
-{
-    bool mousePressed;
-    int  mouseCoordX;
-    int  mouseCoordY;
-};
-
 class PsSPI
 {
+
 public:
-
-    tool_t addTool (PsSPI_Tool tool);
-
-    PsSPI_Event getEvent ();
-
-    void setPixel (int x, int y, PsSPI_Color);
+    virtual void addTool (const char* png) = 0;
 };
 
-PsSPI* getPsSPI ();
-
-void loadPlugin ();
+extern "C"
+{
+    PsSPI* getPsSPI ();
+    void loadPlugin ();
+}
 
 
 #endif /* __PsSPI__ */
