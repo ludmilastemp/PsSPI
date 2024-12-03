626f258eefb47000a9f9eeb65f7182891fabdbea
LudmilaStemp <StemporzheckayaLudmila@yandex.ru>
2024-12-03 03:30:56 +0300

    add tool action

--- PsSPI.hpp
+++ PsSPI.hpp
@@ -1,18 +1,34 @@
 #ifndef __PsSPI__
 #define __PsSPI__
 
-class PsSPI
-{
-
-public:
-    virtual void addTool (const char* png) = 0;
-};
-
 extern "C"
 {
+    struct PsSPI;
+    struct PsSPI_Tool;
     PsSPI* getPsSPI ();
     void loadPlugin ();
 }
 
+using tool_t = int;
+
+struct PsSPI_Tool
+{
+    PsSPI_Tool(const char* init_img, const char* init_name) 
+        : img(init_img), name(init_name) 
+    {}
+
+    tool_t id = -1;
+    const char* img;
+    const char* name;
+    virtual void apply()      {};
+    virtual void activate()   {};
+    virtual void deactivate() {};
+};
+
+class PsSPI
+{
+public:
+    virtual tool_t addTool (PsSPI_Tool* tool) = 0;
+};
 
 #endif /* __PsSPI__ */
--- test.cpp
+++ test.cpp
@@ -1,9 +1,39 @@
 #include "PsSPI.hpp"
 
+#include <iostream>
+
 PsSPI* psspi = nullptr;
+
+struct ToolTest : PsSPI_Tool
+{
+    ToolTest(const char* img, const char* name) 
+        : PsSPI_Tool(img, name)
+    {}
+
+    virtual void apply()      override;
+    virtual void activate()   override;
+    virtual void deactivate() override;
+};
 
 void loadPlugin ()
 {
     psspi = getPsSPI ();
-    psspi->addTool ("img/brush.png");
+
+    ToolTest* tool = new ToolTest { "img/brush.png", "brush" };
+    tool->id = psspi->addTool (tool);
+}
+
+void ToolTest::apply ()
+{
+    std::cout << "la-la-la" << std::endl;
+}
+
+void ToolTest::activate ()
+{
+    std::cout << "meow" << std::endl;
+}
+
+void ToolTest::deactivate ()
+{
+    std::cout << "bye" << std::endl;
 }
